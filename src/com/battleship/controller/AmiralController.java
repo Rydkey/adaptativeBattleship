d85f72708ef3a06d52050237b455d7ae196ecfd2
rydkey <remyrisser@hotmail.fr>
2018-05-07 10:17:14 +0200

    can get row and column

--- src/com/battleship/controller/AmiralController.java
+++ src/com/battleship/controller/AmiralController.java
@@ -3,18 +3,23 @@ package com.battleship.controller;
 import com.battleship.model.Equipe;
 import com.battleship.model.Partie;
 import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 //import com.battleship.vue.AmiralView;
 
-public class AmiralController implements EventHandler<MouseEvent>
-{
+public class AmiralController extends BaseController implements Initializable {
 
   public AnchorPane anchorPane;
   public Rectangle cuirasseRectangle;
@@ -30,26 +35,54 @@ public class AmiralController implements EventHandler<MouseEvent>
   public Rectangle sousMarinRectangle4;
   public Text messageContainer;
   public Button quit;
-  //  private AmiralView amiralView;
   private Partie partie;
   private Equipe equipe;
-
-  @Override
-  public void handle(MouseEvent event)
-  {
-    System.out.println("mouse click detected! " + event.getSource());
-  }
 
   public void endGame()
   {
     System.out.println("end");
   }
 
-  public void click(MouseEvent event)
+//  public EventHandler<MouseEvent> click()
+//  {
+//    EventHandler<MouseEvent> mousePositionHandler = new EventHandler<MouseEvent>() {
+//      @Override
+//      public void handle(MouseEvent event)
+//      {
+//        Node source = (Node) event.getSource();
+//        Integer colIndex = GridPane.getColumnIndex(source);
+//        Integer rowIndex = GridPane.getRowIndex(source);
+//        System.out.println("col : " + colIndex);
+//        event.consume();
+//      }
+//    };
+//    return mousePositionHandler;
+//  }
+
+  public EventHandler<MouseEvent> click()
   {
-    Node source = (Node)event.getSource() ;
-    Integer colIndex = GridPane.getColumnIndex(source);
-    Integer rowIndex = GridPane.getRowIndex(source);
-    System.out.printf(""+colIndex);
+    EventHandler<MouseEvent> mousePositionHandler = new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event)
+      {
+        Node source = (Node) event.getSource();
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.println("col : " + colIndex);
+        event.consume();
+      }
+    };
+    return mousePositionHandler;
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources)
+  {
+    for (int i = 0; i < NB_CASES; i++)
+      for (int j = 0; j < NB_CASES; j++) {
+        Pane pane = new Pane();
+        pane.setOnMouseClicked(this.click());
+        gameMainGrid.add(pane, i, j);
+      }
   }
 }
---
+++ src/com/battleship/controller/BaseController.java
@@ -0,0 +1,5 @@
+package com.battleship.controller;
+
+public class BaseController {
+  public static final int NB_CASES = 10;
+}
