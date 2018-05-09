2f352dbc0ffaa6b23d6a1b35dd76380163c3cca9
rydkey <remyrisser@hotmail.fr>
2018-05-09 15:35:14 +0200

    placement phase sans vérification des coins

--- src/com/battleship/controller/AmiralPlacementController.java
+++ src/com/battleship/controller/AmiralPlacementController.java
@@ -16,6 +16,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
@@ -41,6 +42,7 @@ public class AmiralPlacementController extends BaseController implements Initial
   private Equipe equipe;
   private HashMap<Rectangle, Navire> navireRectangleAssociation;
   private HashMap<Pane, Case> paneCaseAssociation;
+  private ArrayList<Rectangle> unselectableShip;
   private Rectangle shipSelected;
   private boolean orientation;
 
@@ -69,7 +71,44 @@ public class AmiralPlacementController extends BaseController implements Initial
   public EventHandler<MouseEvent> click()
   {
     return event -> {
-
+      if (shipSelected != null) {
+        Navire navire = navireRectangleAssociation.get(shipSelected);
+        Node source = (Node) event.getSource();
+        ArrayList<Pane> tempPaneList = new ArrayList<>();
+        boolean positionable = true;
+        for (int i = 0; i < navire.getTaille(); i++) {
+          /*récupère le pane sous la souris (si le pane est en 1:1, il faut récuperer le pane 11)*/
+          int j;
+          if (orientation) {
+            if (GridPane.getColumnIndex(source) + navire.getTaille() < 8) {
+              for (j = 0; j < navire.getTaille(); j++) {
+                Pane pane = (Pane) gameMainGrid.getChildren().get((GridPane.getColumnIndex(source) + j) * 10 + (GridPane.getRowIndex(source) + 1));
+                tempPaneList.add(pane);
+                positionable = caseIsAcceccible(pane);
+                if (!positionable) break;
+              }
+            }
+          } else {
+            if (GridPane.getRowIndex(source) + navire.getTaille() < 8) {
+              for (j = 0; j < navire.getTaille(); j++) {
+                Pane pane = (Pane) gameMainGrid.getChildren().get(GridPane.getColumnIndex(source) * 10 + (GridPane.getRowIndex(source) + 1 + j));
+                tempPaneList.add(pane);
+                positionable = caseIsAcceccible(pane);
+                if (!positionable) break;
+              }
+            }
+          }
+        }
+        if (positionable) {
+          for (Pane pane : tempPaneList) {
+            shipSelected.setFill(Color.GREY);
+            paneCaseAssociation.get(pane).setStatus(Status.NAVIRE);
+            unselectableShip.add(shipSelected);
+          }
+          shipSelected = null;
+          refreshColor();
+        }
+      }
     };
   }
 
@@ -80,12 +119,10 @@ public class AmiralPlacementController extends BaseController implements Initial
   {
     return event -> {
       if (shipSelected != null) {
-        emptyFill();
         Navire navire = navireRectangleAssociation.get(shipSelected);
         Node source = (Node) event.getSource();
         for (int i = 0; i < navire.getTaille(); i++) {
           /*récupère le pane sous la souris (si le pane est en 1:1, il faut récuperer le pane 11)*/
-          Pane temp = (Pane) gameMainGrid.getChildren().get(GridPane.getColumnIndex(source) * 10 + (GridPane.getRowIndex(source) + 1));
           int j = 0;
           if (orientation) {
             if (GridPane.getColumnIndex(source) + navire.getTaille() > 8) {
@@ -97,7 +134,11 @@ public class AmiralPlacementController extends BaseController implements Initial
             } else {
               for (j = 0; j < navire.getTaille(); j++) {
                 Pane tempA = (Pane) gameMainGrid.getChildren().get((GridPane.getColumnIndex(source) + j) * 10 + (GridPane.getRowIndex(source) + 1));
-                tempA.setStyle("-fx-background-color: purple");
+                if (caseIsAcceccible(tempA)) {
+                  tempA.setStyle("-fx-background-color: purple");
+                } else {
+                  tempA.setStyle("-fx-background-color: red");
+                }
               }
             }
           } else {
@@ -110,13 +151,31 @@ public class AmiralPlacementController extends BaseController implements Initial
             } else {
               for (j = 0; j < navire.getTaille(); j++) {
                 Pane tempA = (Pane) gameMainGrid.getChildren().get(GridPane.getColumnIndex(source) * 10 + (GridPane.getRowIndex(source) + 1 + j));
-                tempA.setStyle("-fx-background-color: purple");
+                if (caseIsAcceccible(tempA)) {
+                  tempA.setStyle("-fx-background-color: purple");
+                } else {
+                  tempA.setStyle("-fx-background-color: red");
+                }
               }
             }
           }
         }
       }
     };
+  }
+
+  private boolean caseIsAcceccible(Pane tempA)
+  {
+    boolean result;
+    switch (paneCaseAssociation.get(tempA).getStatus()) {
+      case VIDE:
+        result = true;
+        break;
+      default:
+        result = false;
+        break;
+    }
+    return result;
   }
 
   /*
@@ -126,7 +185,7 @@ public class AmiralPlacementController extends BaseController implements Initial
   {
     Rectangle clicked = (Rectangle) event.getSource();
     Navire navireCorrespondant = navireRectangleAssociation.get(clicked);
-    if (navireCorrespondant != null) {
+    if (navireCorrespondant != null && !unselectableShip.contains(clicked)) {
       if (shipSelected != null) {
         shipSelected.setFill(Color.BLUE);
       }
@@ -151,6 +210,7 @@ public class AmiralPlacementController extends BaseController implements Initial
   {
     navireRectangleAssociation = new HashMap<>();
     paneCaseAssociation = new HashMap<>();
+    unselectableShip = new ArrayList<>();
     orientation = false;
     for (int i = 0; i < NB_CASES; i++)
       for (int j = 0; j < NB_CASES; j++) {
@@ -159,7 +219,7 @@ public class AmiralPlacementController extends BaseController implements Initial
         paneCaseAssociation.put(pane, laCase);
         pane.setOnMouseClicked(this.click());
         pane.setOnMouseEntered(this.drawShipPrevisqion());
-        pane.setOnMouseExited(this.emptyFill());
+        pane.setOnMouseExited(this.refreshColor());
         gameMainGrid.add(pane, i, j);
       }
     Cuirasse c1 = new Cuirasse();
@@ -184,13 +244,17 @@ public class AmiralPlacementController extends BaseController implements Initial
     navireRectangleAssociation.put(sousMarinRectangle4, s4);
   }
 
-  private EventHandler<MouseEvent> emptyFill()
+  private EventHandler<MouseEvent> refreshColor()
   {
     return event -> {
       for (Node childen : gameMainGrid.getChildren()) {
         Case tempCase = paneCaseAssociation.get(childen);
-        if (tempCase != null && tempCase.getStatus() == Status.VIDE) {
-          childen.setStyle("-fx-color: white");
+        if (tempCase != null) {
+          if (caseIsAcceccible((Pane) childen)) {
+            childen.setStyle("-fx-color: white");
+          } else {
+            childen.setStyle("-fx-background-color: grey");
+          }
         }
       }
     };
--- src/com/battleship/controller/BaseController.java
+++ src/com/battleship/controller/BaseController.java
@@ -2,4 +2,5 @@ package com.battleship.controller;
 
 public class BaseController {
   public static final int NB_CASES = 10;
+
 }
